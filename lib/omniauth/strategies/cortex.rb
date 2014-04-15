2d9e92f5a1615995aae13128b95df34b31b93aa7
Bennett Goble <bennett.goble@careerbuilder.com>
2014-04-15 16:55:33 -0400

    Corrected URLs, less user info transformations

--- lib/omniauth/strategies/cortex.rb
+++ lib/omniauth/strategies/cortex.rb
@@ -6,30 +6,29 @@ module OmniAuth
       option :name, 'cortex'
 
       option :client_options, {
-          :site => 'http://stg.api.cbcortex.com/api/v1',
-          :authorize_url => 'http://stg.api.cbcortex.com/oauth/authorize',
-          :token_url => 'http://stg.api.cbcortex.com/oauth/access_token'
+        :site          => 'https://cbcortex.com/api/v1',
+        :authorize_url => 'https://cbcortex.com/oauth/authorize',
+        :token_url     => 'https://cbcortex.com/oauth/token'
       }
 
       uid{ raw_info['id'] }
 
       info do
         {
-            :nickname => raw_info['email'],
-            :email => raw_info['email'],
-            :name => raw_info['fullname'],
-            :first_name => raw_info['firstname'],
-            :last_name => raw_info['lastname'],
-            :image => raw_info['gravatar']
+          :email      => raw_info['email'],
+          :full_name  => raw_info['fullname'],
+          :first_name => raw_info['firstname'],
+          :last_name  => raw_info['lastname'],
+          :image      => raw_info['gravatar']
         }
       end
 
       extra do
-        {:raw_info => raw_info}
+        { :raw_info => raw_info }
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/users/me').parsed
+        @raw_info ||= access_token.get('users/me').parsed
       end
     end
   end
